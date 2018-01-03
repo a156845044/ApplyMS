9aa7c28e55459e2f56ad40ef14e06ba06e9bc1b7
俨然了一个大世界 <156845044@qq.com>
2018-01-03 22:08:21 +0800

    add
    添加项目

---
+++ ApplyMS/login.aspx
@@ -0,0 +1,49 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="login.aspx.cs" Inherits="ApplyMS.login" %>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width,minimum-scale=1.0,maximum-scale=1.0,initial-scale=1.0,user-scalable=no">
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <title>管理员登录</title>
+    <link href="css/style.css" rel="stylesheet" type="text/css" />
+    <script src="js/jquery.min.js" type="text/javascript"></script>
+</head>
+<body class="loginbody">
+    <form id="form1" runat="server">
+    <div style="width: 100%; height: 100%; min-width: 300px; min-height: 260px;">
+    </div>
+    <div class="login-wrap">
+        <div class="login-logo">
+            LOGO</div>
+        <div class="login-form">
+            <div class="col">
+                <asp:TextBox ID="txtUserName" runat="server" CssClass="login-input" placeholder="账号"
+                    title="账号"></asp:TextBox>
+                <label class="icon user" for="txtUserName">
+                </label>
+            </div>
+            <div class="col">
+                <asp:TextBox ID="txtPassword" runat="server" CssClass="login-input" TextMode="Password"
+                    placeholder="密码" title="密码"></asp:TextBox>
+                <label class="icon pwd" for="txtPassword">
+                </label>
+            </div>
+            <div class="col">
+                <asp:Button ID="btnSubmit" runat="server" Text="登 录" CssClass="login-btn" />
+            </div>
+        </div>
+        <div class="login-tips">
+            <i></i>
+            <p id="msgtip" runat="server">
+                请输入用户名和密码</p>
+        </div>
+    </div>
+    <div class="copy-right">
+        <p>
+            版权所有 Copyright © 2016 All Rights Reserved.</p>
+    </div>
+    </form>
+</body>
+</html>
---
+++ ApplyMS/webs/inc/Site.Master.designer.cs
@@ -0,0 +1,105 @@
+﻿//------------------------------------------------------------------------------
+// <自动生成>
+//     此代码由工具生成。
+//
+//     对此文件的更改可能会导致不正确的行为，并且如果
+//     重新生成代码，这些更改将会丢失。 
+// </自动生成>
+//------------------------------------------------------------------------------
+
+namespace ApplyMS.webs.inc {
+    
+    
+    public partial class Site {
+        
+        /// <summary>
+        /// Head1 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlHead Head1;
+        
+        /// <summary>
+        /// Link1 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlLink Link1;
+        
+        /// <summary>
+        /// Link2 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlLink Link2;
+        
+        /// <summary>
+        /// Link3 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlLink Link3;
+        
+        /// <summary>
+        /// Link4 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlLink Link4;
+        
+        /// <summary>
+        /// Link5 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlLink Link5;
+        
+        /// <summary>
+        /// HeadContent 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder HeadContent;
+        
+        /// <summary>
+        /// form1 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// PageContent 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder PageContent;
+        
+        /// <summary>
+        /// ScriptContent 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder ScriptContent;
+    }
+}
